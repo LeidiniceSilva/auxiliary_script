49973dc7e2bbbb3170fceb8f226a50c113eae3aa
Leidinice Silva <leidinicesilva@gmail.com>
2023-06-25 15:28:21 -0300

    update scripts

--- convert_csv2nc_cru.py
+++ convert_csv2nc_cru.py
@@ -15,55 +15,55 @@ import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 from dict_stations_inmet import inmet
 
-# create date list
-dt = pd.date_range('1978-01-01','2020-12-31', freq='M').strftime('%Y-%m-%d').tolist()
+# ~ # create date list
+# ~ dt = pd.date_range('1978-01-01','2020-12-31', freq='M').strftime('%Y-%m-%d').tolist()
 																																																											
-# This load the file (date, variable)
-data = np.loadtxt(os.path.join('/home/nice/Downloads', 'PRE_PET_PARANA_CIC_78_20_CRU405_csv.csv'), dtype='str', delimiter=';', unpack=True)
-data = data[:,1:]
-data_values = np.where(data[4,:] == 'null', -999., data[4,:])
-data_values = np.array(data_values, dtype=float)
+# ~ # This load the file (date, variable)
+# ~ data = np.loadtxt(os.path.join('/home/nice/Downloads', 'PRE_PET_PARANA_CIC_78_20_CRU405_csv.csv'), dtype='str', delimiter=';', unpack=True)
+# ~ data = data[:,1:]
+# ~ data_values = np.where(data[4,:] == 'null', -999., data[4,:])
+# ~ data_values = np.array(data_values, dtype=float)
 
-data_dates  = []
-for i in range(len(dt)):
+# ~ data_dates  = []
+# ~ for i in range(len(dt)):
 		
-	data_dates.append('{0}'.format(dt[i]))
+	# ~ data_dates.append('{0}'.format(dt[i]))
 
-	print('Date organized:', data_dates[i], data_values[i])
+	# ~ print('Date organized:', data_dates[i], data_values[i])
 
-nc_output = '/home/nice/Downloads/pre_parana_CRU405_mon_1978-2020.nc'
+# ~ nc_output = '/home/nice/Downloads/pre_parana_CRU405_mon_1978-2020.nc'
 
-# create netcdf
-ds = Dataset(nc_output, mode='w', format='NETCDF4_CLASSIC')
+# ~ # create netcdf
+# ~ ds = Dataset(nc_output, mode='w', format='NETCDF4_CLASSIC')
 
-ds.Conventions 	= 'CF-1.6'
-ds.title 		= 'CRU TS4.05 Precipitation'
-ds.institution 	= 'Data held at British Atmospheric Data Centre, RAL, UK'
-ds.source 		= 'Run ID = 2103051243. Data generated from:pre.2103041709.dtb'
-ds.history 		= 'Rewrote via python script'
-ds.references 	= 'Information on the data is available at http://badc.nerc.ac.uk/data/cru/'
-ds.comment 		= 'This script convert .csv to .nc'
+# ~ ds.Conventions 	= 'CF-1.6'
+# ~ ds.title 		= 'CRU TS4.05 Precipitation'
+# ~ ds.institution 	= 'Data held at British Atmospheric Data Centre, RAL, UK'
+# ~ ds.source 		= 'Run ID = 2103051243. Data generated from:pre.2103041709.dtb'
+# ~ ds.history 		= 'Rewrote via python script'
+# ~ ds.references 	= 'Information on the data is available at http://badc.nerc.ac.uk/data/cru/'
+# ~ ds.comment 		= 'This script convert .csv to .nc'
 	
-ds.createDimension('time', None)
+# ~ ds.createDimension('time', None)
 
-time 				= ds.createVariable('time', float, ('time'))
-time.axis 			= 'L'
-time.calendar 		= '360_day'
-time.units			= 'months since 1978-01-01'
-time[:]				= range(len(data_dates))
+# ~ time 				= ds.createVariable('time', float, ('time'))
+# ~ time.axis 			= 'L'
+# ~ time.calendar 		= '360_day'
+# ~ time.units			= 'months since 1978-01-01'
+# ~ time[:]				= range(len(data_dates))
 
-var 				= ds.createVariable('pre',  float, ('time'))
-var.units 			= 'mm'
-var.long_name 		= 'Precipitation'
-var.standard_name 	= 'Precipitation'
-var.missing_value 	= -999
-var[:] 				= data_values[:]
+# ~ var 				= ds.createVariable('pre',  float, ('time'))
+# ~ var.units 			= 'mm'
+# ~ var.long_name 		= 'Precipitation'
+# ~ var.standard_name 	= 'Precipitation'
+# ~ var.missing_value 	= -999
+# ~ var[:] 				= data_values[:]
 	
-ds.close()
+# ~ ds.close()
 
-if os.path.exists(nc_output): 
-	print('Done -->', nc_output)
-exit()
+# ~ if os.path.exists(nc_output): 
+	# ~ print('Done -->', nc_output)
+# ~ exit()
 
 # open netcdf
 dn = xr.open_dataset('/home/nice/Downloads/' + 'pet_parana_CRU405_mon_1978-2020.nc')
